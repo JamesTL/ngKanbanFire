2950c35fff1bca0ba19b290cc220dd107bafaa0c
James Love <james.love1@equatex.comm>
2022-02-16 21:02:58 +0000

    tutorial to step 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule , NO_ERRORS_SCHEMA} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { TaskComponent } from './task/task.component';
+
+const importedMatComponents = [MatToolbarModule, MatIconModule, MatCardModule, DragDropModule ]
+
 @NgModule({
+  schemas:[ NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TaskComponent,
+  ],    
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatCardModule, DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
